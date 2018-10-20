b83d3a08e1d8af87683b58cf9768c6e550b0f241
Supak Tangyourg <ninkoman@gmail.com>
2018-10-20 13:53:07 +0700

    Initial commit

---
+++ java/co/advancedlogic/thainationalidcard/SmartCardDevice.java
@@ -0,0 +1,424 @@
+package co.advancedlogic.thainationalidcard;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
+public final class SmartCardDevice {
+    private static final String ACTION_USB_PERMISSION = "ninkoman.smartcardreader.USB_PERMISSION";
+    private static final String TAG = "SmartCardDevice";
+
+    private Context context;
+    private UsbDevice device;
+    private SmartCardMessage message;
+    private PendingIntent mPermissionIntent;
+    private boolean havePermission = false;
+    private UsbDeviceConnection deviceConnection = null;
+    private UsbInterface deviceInterface = null;
+    private UsbEndpoint inputEndpoint = null;
+    private UsbEndpoint outputEndpoint = null;
+
+    private int infIndex = 0;
+    private int endpointInputIndex = 0;
+    private int endpointOutputIndex = 0;
+    private boolean stopped = true;
+    private boolean deviceDetachedRegister = false;
+
+    private SmartCardDeviceEvent eventCallback = null;
+
+    public SmartCardDevice(Context context, UsbDevice device, int infIndex, int endpointInputIndex, int endpointOutputIndex, SmartCardDeviceEvent eventCallback) {
+        if (context == null || device == null) {
+            throw new NullPointerException();
+        }
+
+        this.context = context;
+        this.device = device;
+        this.message = new SmartCardMessage();
+
+        this.infIndex = infIndex;
+        this.endpointInputIndex = endpointInputIndex;
+        this.endpointOutputIndex = endpointOutputIndex;
+        this.eventCallback = eventCallback;
+
+        mPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        context.registerReceiver(this.mUsbPermissionReceiver, filter);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        this.stop();
+    }
+
+    public interface SmartCardDeviceEvent {
+        void OnReady(SmartCardDevice device);
+        void OnDetached(SmartCardDevice device);
+    }
+
+    public static SmartCardDevice getSmartCardDevice(Context context, String nameContain, int infIndex, int endpointInputIndex, int endpointOutputIndex, SmartCardDeviceEvent eventCallback) {
+        UsbManager manager;
+        HashMap<String, UsbDevice> deviceList;
+        UsbDevice device = null;
+        SmartCardDevice cardDevice = null;
+
+        manager = (UsbManager)context.getSystemService(Context.USB_SERVICE);
+        if (manager == null) {
+            Log.w(TAG, "USB manager not found");
+            return null;
+        }
+
+        deviceList = manager.getDeviceList();
+        if (deviceList == null) {
+            Log.w(TAG, "USB device list not found");
+            return null;
+        }
+
+        for (String key : deviceList.keySet()) {
+            Log.d(TAG, "Search device contain name [" + nameContain + "] with [" + deviceList.get(key).getProductName() + "] [" + deviceList.get(key).getDeviceName() + "]");
+            if (deviceList.get(key).getProductName().contains(nameContain)) {
+                device = deviceList.get(key);
+                Log.d(TAG, "Found device: " + device.getProductName());
+                break;
+            }
+        }
+
+        if (device == null) {
+            Log.w(TAG, "Device name [" + nameContain + "] not found");
+            return null;
+        }
+
+        cardDevice = new SmartCardDevice(context, device, infIndex, endpointInputIndex, endpointOutputIndex, eventCallback);
+
+        cardDevice.start();
+
+        return cardDevice;
+    }
+
+    public static SmartCardDevice getSmartCardDevice(Context context, String nameContain, SmartCardDeviceEvent eventCallback) {
+        UsbManager manager;
+        HashMap<String, UsbDevice> deviceList;
+        UsbDevice device = null;
+        SmartCardDevice cardDevice = null;
+
+        manager = (UsbManager)context.getSystemService(Context.USB_SERVICE);
+        if (manager == null) {
+            Log.w(TAG, "USB manager not found");
+            return null;
+        }
+
+        deviceList = manager.getDeviceList();
+        if (deviceList == null) {
+            Log.w(TAG, "USB device list not found");
+            return null;
+        }
+
+        for (String key : deviceList.keySet()) {
+            Log.d(TAG, "Search device contain name [" + nameContain + "] in [" + deviceList.get(key).getProductName() + "] [" + deviceList.get(key).getDeviceName() + "]");
+            if (deviceList.get(key).getProductName().contains(nameContain)) {
+                device = deviceList.get(key);
+                Log.d(TAG, "Found device: " + device.getProductName());
+                break;
+            }
+        }
+
+        if (device == null) {
+            Log.w(TAG, "Device name [" + nameContain + "] not found");
+            return null;
+        }
+
+        UsbInterface intf = device.getInterface(0);
+        int[] endpointIndex = new int[2];
+        int index = 0;
+
+        for (int i = 0; i < intf.getEndpointCount(); i++) {
+            if (intf.getEndpoint(i).getAttributes() == 2) {
+                endpointIndex[index] = i;
+                index++;
+
+                if (index >= 2) {
+                    break;
+                }
+            }
+        }
+        if (index < 2) {
+            Log.d(TAG, "Smart Card device endpoint detect failed");
+            return null;
+        }
+
+        cardDevice = new SmartCardDevice(context, device, 0, endpointIndex[0], endpointIndex[1], eventCallback);
+
+        cardDevice.start();
+
+        return cardDevice;
+    }
+
+    public void start() {
+        UsbManager manager = (UsbManager)this.context.getSystemService(Context.USB_SERVICE);
+        if (manager != null) {
+            manager.requestPermission(device, mPermissionIntent);
+        } else {
+            throw new RuntimeException("USB manager not found");
+        }
+    }
+
+    public void stop() {
+        if (!this.stopped) {
+            //this.deviceConnection.close();
+            this.stopped = true;
+        }
+
+        this.havePermission = false;
+    }
+
+    //public void setMessageTypeT1(boolean isT1) {
+    //    this.message.setMessageTypeT1(isT1);
+    //}
+
+    private final BroadcastReceiver mUsbPermissionReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            UsbManager manager;
+
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                    if (device != null && device.getDeviceName().equals(SmartCardDevice.this.device.getDeviceName())) {
+                        if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                            manager = (UsbManager)context.getSystemService(Context.USB_SERVICE);
+                            if (manager == null) {
+                                throw new RuntimeException("USB manager not found");
+                            }
+
+                            SmartCardDevice.this.deviceConnection = manager.openDevice(device);
+
+                            if (SmartCardDevice.this.deviceConnection == null) {
+                                throw new RuntimeException("Invalid USB device connection");
+                            }
+
+                            SmartCardDevice.this.deviceInterface = device.getInterface(infIndex);
+                            SmartCardDevice.this.inputEndpoint = SmartCardDevice.this.deviceInterface.getEndpoint(endpointInputIndex);
+                            SmartCardDevice.this.outputEndpoint = SmartCardDevice.this.deviceInterface.getEndpoint(endpointOutputIndex);
+
+                            if (SmartCardDevice.this.deviceInterface == null || SmartCardDevice.this.inputEndpoint == null || SmartCardDevice.this.outputEndpoint == null) {
+                                throw new RuntimeException("Invalid USB device interface or endpoint");
+                            }
+
+                            SmartCardDevice.this.havePermission = true;
+                            SmartCardDevice.this.stopped = false;
+
+                            if (SmartCardDevice.this.eventCallback != null)
+                                SmartCardDevice.this.eventCallback.OnReady(SmartCardDevice.this);
+                            Log.d(TAG, "Card device is ready");
+
+                            if (!SmartCardDevice.this.deviceDetachedRegister) {
+                                IntentFilter filter = new IntentFilter(UsbManager.ACTION_USB_DEVICE_DETACHED);
+                                SmartCardDevice.this.context.registerReceiver(SmartCardDevice.this.mUsbDetachedReceiver, filter);
+                                SmartCardDevice.this.deviceDetachedRegister = true;
+                            }
+                        } else {
+                            SmartCardDevice.this.havePermission = false;
+                            throw new RuntimeException("Device is not granted");
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    private final BroadcastReceiver mUsbDetachedReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action != null && action.equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
+                UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                Log.d(TAG, "USB device is detached");
+
+                if (device.getDeviceName().equals(SmartCardDevice.this.device.getDeviceName())) {
+                    if (SmartCardDevice.this.eventCallback != null)
+                        SmartCardDevice.this.eventCallback.OnDetached(SmartCardDevice.this);
+                    SmartCardDevice.this.stop();
+
+                    SmartCardDevice.this.context.unregisterReceiver(SmartCardDevice.this.mUsbDetachedReceiver);
+                    SmartCardDevice.this.deviceDetachedRegister = false;
+                }
+            }
+        }
+    };
+
+    public SmartCardMessage.DataBlock getATR() {
+        if (!this.havePermission) {
+            Log.w(TAG, "USB permission require, please call start() first");
+            return null;
+        }
+
+        byte[] message = this.message.getMessageIccPowerOn();
+
+        if (!this.prepareConnection()) {
+            Log.w(TAG, "prepareConnection() failed");
+            return null;
+        }
+
+        if (!this.sendRequestMessage(message)) {
+            Log.w(TAG, "sendRequestMessage() failed");
+            return null;
+        }
+
+        try {
+            if ((message = this.receiveResponseMessage()) == null) {
+                Log.w(TAG, "receiveResponseMessage() error");
+                return null;
+            }
+        } catch (IOException e) {
+            Log.w(TAG, "receiveResponseMessage() failed");
+            return null;
+        }
+
+        return this.message.parseDataBlock(message);
+    }
+
+    public SmartCardMessage.DataBlock sendAPDU(byte[] dataAPDU) {
+        byte[] data;
+
+        if (!this.havePermission) {
+            Log.w(TAG, "USB permission require, please call start() first");
+            return null;
+        }
+
+        byte[][] messages = this.message.getMessageXfrBlock(dataAPDU);
+
+        if (messages == null) {
+            Log.w(TAG, "getMessageXfrBlock() return failed");
+            return null;
+        }
+
+        if (!this.prepareConnection()) {
+            Log.w(TAG, "prepareConnection() failed");
+            return null;
+        }
+
+        for (byte[] message: messages) {
+            if (!this.sendRequestMessage(message)) {
+                Log.w(TAG, "sendRequestMessage() error");
+                return null;
+            }
+        }
+
+        try {
+            if ((data = this.receiveResponseMessage()) == null) {
+                Log.w(TAG, "receiveResponseMessage() error");
+                return null;
+            }
+        } catch (IOException e) {
+            Log.w(TAG, "receiveResponseMessage() failed");
+            return null;
+        }
+
+        return this.message.parseDataBlock(data);
+    }
+
+    private boolean prepareConnection() {
+        if (!this.havePermission) {
+            Log.w(TAG, "USB permission require, please call start() first");
+            return false;
+        }
+
+        this.deviceConnection.claimInterface(this.deviceInterface, true);
+
+        return true;
+    }
+
+    private boolean sendRequestMessage(byte[] message) {
+        int length;
+
+        if (!this.havePermission) {
+            Log.w(TAG, "USB permission require, please call start() first");
+            return false;
+        }
+
+        if (message == null || message.length == 0) {
+            Log.w(TAG, "message is null or invalid length");
+            return false;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b: message) {
+            sb.append(String.format("%02x", (byte)b));
+        }
+        Log.d(TAG, "Sending message [" + message.length + "][" + sb.toString() + "]");
+
+        length = this.deviceConnection.bulkTransfer(this.inputEndpoint, message, message.length, 0);
+
+        if (length <= 0 || length != message.length) {
+            Log.w(TAG, "message sending return invalid length " + length + "/" + message.length);
+            return false;
+        }
+
+        return true;
+    }
+
+    private byte[] receiveResponseMessage() throws IOException {
+        int length, totalLength;
+        byte[] buffer;
+        ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
+
+        if (!this.havePermission) {
+            Log.w(TAG, "USB permission require, please call start() first");
+            return null;
+        }
+
+        buffer = new byte[this.outputEndpoint.getMaxPacketSize()];
+
+        length = this.deviceConnection.bulkTransfer(this.outputEndpoint, buffer, buffer.length, 0);
+
+        if (length < 10) {
+            Log.w(TAG, "receive message invalid length " + length);
+            return null;
+        }
+
+        dataStream = new ByteArrayOutputStream();
+        dataStream.write(buffer, 0, length);
+
+        totalLength = (((int)buffer[4] & 0xff) << 24) | (((int)buffer[3] & 0xff) << 16) | (((int)buffer[2] & 0xff) << 8) | ((int)buffer[1] & 0xff);
+        totalLength += 10;
+
+        while (dataStream.size() < totalLength) {
+            length = this.deviceConnection.bulkTransfer(this.outputEndpoint, buffer, buffer.length, 0);
+            if (length < 0) {
+                Log.w(TAG, "receive continues message return error");
+                dataStream.close();
+                return null;
+            } else if (length == 0) {
+                if (dataStream.size() < totalLength) {
+                    Log.w(TAG, "receive continues message not success");
+                    dataStream.close();
+                    return null;
+                } else {
+                    break;
+                }
+            }
+
+            dataStream.write(buffer, 0, length);
+        }
+
+        buffer = dataStream.toByteArray();
+        dataStream.close();
+
+        return buffer;
+    }
+}
---
+++ java/co/advancedlogic/thainationalidcard/SmartCardMessage.java
@@ -0,0 +1,209 @@
+package co.advancedlogic.thainationalidcard;
+
+import android.util.Log;
+
+public final class SmartCardMessage {
+    private static final String TAG = "SmartCardMessage";
+
+    private boolean isT1 = false;
+    private int slotNumber = 0;
+    private int sequence = 0x00;
+    private int timeout = 0;
+    private int exchangeLevel = 0;
+    private int maxPacketSize = 64;
+
+    public SmartCardMessage() {
+    }
+
+    public SmartCardMessage(boolean t1) {
+        this.isT1 = t1;
+    }
+
+    public void setMessageTypeT1(boolean t1) {
+        this.isT1 = t1;
+    }
+
+    public boolean setSlotNumber(int slotNumber) {
+        if (slotNumber >= 0 && slotNumber <= 127) {
+            this.slotNumber = slotNumber;
+            return true;
+        }
+
+        Log.w(TAG, "Invalid slot number range");
+        return false;
+    }
+
+    public void setMaxPacketSize(int size) {
+        this.maxPacketSize = size;
+    }
+
+    private void initProperties(byte[] message, int type) {
+        if (message == null) {
+            Log.w(TAG, "message is null");
+            return;
+        }
+
+        message[0] = (byte)type;
+        message[5] = (byte)this.slotNumber;
+        message[6] = (byte)this.sequence;
+        message[7] = (byte)this.timeout;
+        message[8] = (byte)this.exchangeLevel;
+        message[9] = (byte)0x00;
+
+        this.sequence++;
+    }
+
+    private void initMessageLength(byte[] message, int length) {
+        if (message == null) {
+            Log.w(TAG, "message is null");
+            return;
+        }
+
+        message[1] = (byte)(length & 0xff);
+        message[2] = (byte)((length >> 8) & 0xff);
+        message[3] = (byte)((length >> 16) & 0xff);
+        message[4] = (byte)((length >> 24) & 0xff);
+    }
+
+    public byte[] getMessageIccPowerOn() {
+        byte[] message = new byte[10];
+
+        this.initProperties(message,0x62);
+
+        return message;
+    }
+
+    public byte[] getMessageIccPowerOff() {
+        byte[] message = new byte[10];
+
+        this.initProperties(message,0x63);
+
+        return message;
+    }
+
+    public byte[] getMessageGetSlotStatus() {
+        byte[] message = new byte[10];
+
+        this.initProperties(message,0x65);
+
+        return message;
+    }
+
+    public byte[][] getMessageXfrBlock(byte[] data) {
+        byte[][] messages;
+        byte[] block, dataTmp;
+        int blockSize;
+        byte xorSum = 0;
+
+        if (this.isT1) {
+            if (data.length > 254) {
+                Log.w(TAG, "Invalid data block length");
+                return null;
+            }
+            dataTmp = new byte[data.length + 4];
+            dataTmp[2] = (byte)data.length;
+            System.arraycopy(data, 0, dataTmp, 3, data.length);
+            for (byte b: dataTmp) {
+                xorSum ^= b;
+            }
+            dataTmp[dataTmp.length - 1] = xorSum;
+
+            data = dataTmp;
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b: data) {
+                sb.append(String.format("%02x", (byte)b));
+            }
+            Log.d(TAG, "APDU T=1 [" + data.length + "][" + sb.toString() + "]");
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (byte b: data) {
+                sb.append(String.format("%02x", (byte)b));
+            }
+            Log.d(TAG, "APDU T=0 [" + data.length + "][" + sb.toString() + "]");
+        }
+
+        int blockCount = ((data.length + 10) / this.maxPacketSize) + ((((data.length + 10) % this.maxPacketSize) > 0) ? 1:0);
+
+        messages = new byte[blockCount][];
+
+        for (int i = 0; i < blockCount; i++) {
+            blockSize = (((data.length + 10) - (i * this.maxPacketSize)) > this.maxPacketSize) ? this.maxPacketSize:((data.length + 10) - (i * this.maxPacketSize));
+            block = new byte[blockSize];
+
+            if (i == 0) {
+                this.initProperties(block, 0x6f);
+                this.initMessageLength(block, data.length);
+                blockSize = ((data.length + 10) > this.maxPacketSize) ? (this.maxPacketSize - 10):data.length;
+
+                System.arraycopy(data, 0, block, 10, blockSize);
+            } else {
+                blockSize = (((data.length + 10) - (i * this.maxPacketSize)) > this.maxPacketSize) ? this.maxPacketSize:((data.length + 10) - (i * this.maxPacketSize));
+
+                System.arraycopy(data, (i * this.maxPacketSize) - 10, block, 0, blockSize);
+            }
+
+            messages[i] = block;
+        }
+
+        return messages;
+    }
+
+    public DataBlock parseDataBlock(byte[] data) {
+        int length;
+
+        if (data == null) {
+            Log.w(TAG, "data is null");
+            return null;
+        }
+
+        Log.d(TAG, "Parsing data length " + data.length);
+
+        if (data[0] != (byte)0x80) {
+            Log.w(TAG, String.format("Invalid data type [%02x]", (byte)data[0]));
+            return null;
+        }
+
+        DataBlock dataBlock = new DataBlock();
+
+        dataBlock.status = (int)data[7];
+        dataBlock.error = (int)data[8];
+
+        length = (int)((((int)data[4] & 0xff) << 24) | (((int)data[3] & 0xff) << 16 )| (((int)data[2] & 0xff) << 8) | ((int)data[1] & 0xff));
+
+        if (data.length < length + 10) {
+            Log.w(TAG, String.format("Invalid data length %d/%d", data.length, length + 10));
+            return null;
+        }
+
+        if (length > 0) {
+            if (data[10] == (byte)0x3b) {
+                dataBlock.dataType = DataType.ATR;
+            } else if (data[10] == (byte)0x61 || data[10] == (byte)0x6c) {
+                dataBlock.dataType = DataType.APDU_HEADER;
+            } else {
+                dataBlock.dataType = DataType.APDU_DATA;
+            }
+        }
+
+        dataBlock.data = new byte[length];
+
+        System.arraycopy(data, 10, dataBlock.data, 0, length);
+
+        return dataBlock;
+    }
+
+    public enum DataType {
+        UNKNOWN,
+        ATR,
+        APDU_HEADER,
+        APDU_DATA
+    }
+
+    public class DataBlock {
+        public DataType dataType = DataType.UNKNOWN;
+        public byte[] data = null;
+        public int status = 0;
+        public int error = 0;
+    }
+}
