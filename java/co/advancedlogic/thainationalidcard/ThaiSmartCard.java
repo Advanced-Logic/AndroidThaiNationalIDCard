f9de80aa694b4edb968154e765e3db979243268e
Supak Tangyourg <ninkoman@gmail.com>
2018-10-22 00:56:05 +0700

    Add new command

--- java/co/advancedlogic/thainationalidcard/ThaiSmartCard.java
+++ java/co/advancedlogic/thainationalidcard/ThaiSmartCard.java
@@ -1,9 +1,15 @@
 package co.advancedlogic.thainationalidcard;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.Locale;
 
 public final class ThaiSmartCard {
     private final String TAG = "ThaiSmartCard";
@@ -147,7 +153,7 @@ public final class ThaiSmartCard {
         return this.byteArrayToHexString(data.data, 13, 8);
     }
 
-    public class CardInformation {
+    public class PersonalInformation {
         public String CardInfo;
         public String PersonalID;
         public String NameTH;
@@ -161,9 +167,9 @@ public final class ThaiSmartCard {
         public String ExpireDate;
     }
 
-    public CardInformation getCardInformation() {
+    public PersonalInformation getPersonalInformation() {
         SmartCardMessage.DataBlock data;
-        CardInformation cardInformation;
+        PersonalInformation personalInformation;
 
         byte[] block1_1 = new byte[]{(byte)0x80, (byte)0xb0, (byte)0x00, (byte)0x00, (byte)0x02, (byte)0x00, (byte)0xff};
         byte[] block1_2 = new byte[]{(byte)0x80, (byte)0xb0, (byte)0x00, (byte)0xff, (byte)0x02, (byte)0x00, (byte)0x7a};
@@ -177,36 +183,36 @@ public final class ThaiSmartCard {
         byte[] buffer = new byte[377];
 
         if ((data = this.getCardData(block1_1)) == null) {
-            Log.d(TAG, "Get card information block 1-1 failed");
+            Log.d(TAG, "Get personal information block 1-1 failed");
             return null;
         }
 
         if (data.status != 0 || data.error != 0 || data.data.length != 0xff) {
-            Log.w(TAG, String.format("Invalid card information block 1-1 [%d][%d][%d][%s]", data.status, data.error, data.data.length, this.byteArrayToHexString(data.data)));
+            Log.w(TAG, String.format("Invalid personal information block 1-1 [%d][%d][%d][%s]", data.status, data.error, data.data.length, this.byteArrayToHexString(data.data)));
             return null;
         }
 
         System.arraycopy(data.data, 0, buffer,0, 0xff);
 
         if ((data = this.getCardData(block1_2)) == null) {
-            Log.w(TAG, "Get card information block 1-2 failed");
+            Log.w(TAG, "Get personal information block 1-2 failed");
             return null;
         }
 
         if (data.status != 0 || data.error != 0 || data.data.length != 0x7a) {
-            Log.w(TAG, String.format("Invalid card information block 1-2 [%d][%d][%d][%s]", data.status, data.error, data.data.length, this.byteArrayToHexString(data.data)));
+            Log.w(TAG, String.format("Invalid personal information block 1-2 [%d][%d][%d][%s]", data.status, data.error, data.data.length, this.byteArrayToHexString(data.data)));
             return null;
         }
 
         System.arraycopy(data.data, 0, buffer,0xff, 0x7a);
 
         if ((data = this.getCardData(block2)) == null) {
-            Log.w(TAG, "Get card information block 2 failed");
+            Log.w(TAG, "Get personal information block 2 failed");
             return null;
         }
 
         if (data.status != 0 || data.error != 0 || data.data.length != 0xae) {
-            Log.w(TAG, String.format("Invalid card information block 2 [%d][%d][%d][%s]", data.status, data.error, data.data.length, this.byteArrayToHexString(data.data)));
+            Log.w(TAG, String.format("Invalid personal information block 2 [%d][%d][%d][%s]", data.status, data.error, data.data.length, this.byteArrayToHexString(data.data)));
             return null;
         }
 
@@ -267,25 +273,183 @@ public final class ThaiSmartCard {
         addressBuffer = Arrays.copyOf(addressBuffer, length);
 
         // return object
-        cardInformation = new CardInformation();
+        personalInformation = new PersonalInformation();
 
         try {
-            cardInformation.CardInfo = String.format("%s-%s-%s-%s", new String(Arrays.copyOfRange(buffer, 375, 377), "TIS620"), new String(Arrays.copyOfRange(buffer, 0, 4), "TIS620"), new String(Arrays.copyOfRange(buffer, 226, 237), "TIS620"), new String(Arrays.copyOfRange(buffer, 238, 246), "TIS620"));
-            cardInformation.PersonalID = new String(Arrays.copyOfRange(buffer, 4, 17), "TIS620");
-            cardInformation.NameTH = new String(nameTHBuffer, "TIS620");
-            cardInformation.NameEN = new String(nameENBuffer, "TIS620");
-            cardInformation.BirthDate = new String(Arrays.copyOfRange(buffer, 217, 225), "TIS620");
-            cardInformation.Issuer = new String(issuerBuffer, "TIS620");
-            cardInformation.IssuerCode = new String(Arrays.copyOfRange(buffer, 346, 359), "TIS620");
-            cardInformation.IssueDate = new String(Arrays.copyOfRange(buffer, 359, 367), "TIS620");
-            cardInformation.ExpireDate = new String(Arrays.copyOfRange(buffer, 367, 375), "TIS620");
-            cardInformation.Address = new String(addressBuffer, "TIS620");
-            cardInformation.PictureTag = new String(Arrays.copyOfRange(data.data, 160, 174), "TIS620");
+            personalInformation.CardInfo = String.format("%s-%s-%s-%s", new String(Arrays.copyOfRange(buffer, 375, 377), "TIS620"), new String(Arrays.copyOfRange(buffer, 0, 4), "TIS620"), new String(Arrays.copyOfRange(buffer, 226, 237), "TIS620"), new String(Arrays.copyOfRange(buffer, 238, 246), "TIS620"));
+            personalInformation.PersonalID = new String(Arrays.copyOfRange(buffer, 4, 17), "TIS620");
+            personalInformation.NameTH = new String(nameTHBuffer, "TIS620");
+            personalInformation.NameEN = new String(nameENBuffer, "TIS620");
+            personalInformation.BirthDate = new String(Arrays.copyOfRange(buffer, 217, 225), "TIS620");
+            personalInformation.Issuer = new String(issuerBuffer, "TIS620");
+            personalInformation.IssuerCode = new String(Arrays.copyOfRange(buffer, 346, 359), "TIS620");
+            personalInformation.IssueDate = new String(Arrays.copyOfRange(buffer, 359, 367), "TIS620");
+            personalInformation.ExpireDate = new String(Arrays.copyOfRange(buffer, 367, 375), "TIS620");
+            personalInformation.Address = new String(addressBuffer, "TIS620");
+            personalInformation.PictureTag = new String(Arrays.copyOfRange(data.data, 160, 174), "TIS620");
         } catch (UnsupportedEncodingException e) {
             Log.w(TAG, "Cannot decode TIS620 string");
             return null;
         }
 
-        return cardInformation;
+        return personalInformation;
+    }
+
+    public class ChipCardADM {
+        public String Version;
+        public String State;
+        public String Authorize;
+        public String LaserNumber;
+    }
+
+    public ChipCardADM getChipCardADM() {
+        SmartCardMessage.DataBlock data;
+        ChipCardADM chipCardADM;
+
+        byte[] message = new byte[]{(byte)0x80, (byte)0x00, (byte)0x00, (byte)0x00};
+
+        if (!this.selectAppletExtension()) {
+            Log.d(TAG, "selectAppletExtension fail");
+            return null;
+        }
+
+        if ((data = this.getCardData(message)) == null) {
+            Log.d(TAG, "Get card information block 1-1 failed");
+            return null;
+        }
+
+        if (data.status != 0 || data.error != 0 || data.data.length != 0x17) {
+            Log.w(TAG, String.format("Invalid card ADM response [%d][%d][%d][%s]", data.status, data.error, data.data.length, this.byteArrayToHexString(data.data)));
+            return null;
+        }
+
+        chipCardADM = new ChipCardADM();
+
+        chipCardADM.Version = String.format(Locale.US, "%d.%d", data.data[0], data.data[1]);
+        chipCardADM.State = String.format(Locale.US, "%d", data.data[2]);
+        chipCardADM.Authorize = String.format(Locale.US, "%d", data.data[3]);
+        chipCardADM.LaserNumber = new String(Arrays.copyOfRange(data.data, 7,23));
+
+        return chipCardADM;
+    }
+
+    public Bitmap getPersonalPicture() {
+        SmartCardMessage.DataBlock data;
+        byte[] message = new byte[]{(byte)0x80, (byte)0xb0, (byte)0x00, (byte)0x00, (byte)0x02, (byte)0x00, (byte)0x00};
+        byte[] pictureBuffer;
+
+        if (!this.selectAppletStorageData()) {
+            Log.d(TAG, "selectAppletStorageData fail");
+            return null;
+        }
+
+        int offset = 0x017b, blockNumber = 1, blockLength, index = 0;
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        int PERSONAL_PIC_LENGTH = 5118;
+
+        while (index < PERSONAL_PIC_LENGTH) {
+            blockLength = ((PERSONAL_PIC_LENGTH - index) > 0xff) ? 0xff:(PERSONAL_PIC_LENGTH - index);
+
+            message[2] = (byte)((offset >> 8) & 0xff);
+            message[3] = (byte)(offset & 0xff);
+            message[6] = (byte)blockLength;
+
+            if ((data = this.getCardData(message)) == null) {
+                Log.w(TAG, "Get personal picture block [" + blockNumber + "] failed");
+                return null;
+            }
+
+            if (data.status != 0 || data.error != 0) {
+                Log.w(TAG, String.format("Invalid personal picture response [%d][%d][%d][%d][%s]", blockNumber, data.status, data.error, data.data.length, this.byteArrayToHexString(data.data)));
+                return null;
+            }
+
+            if (data.data.length != blockLength) {
+                Log.w(TAG, "Get personal picture block [" + blockNumber + "] return invalid length [ " + data.data.length + "/" + blockLength + "]");
+                return null;
+            }
+
+            try {
+                buffer.write(data.data);
+            } catch (IOException e) {
+                Log.w(TAG, "Get personal picture block [" + blockNumber + "] append to buffer failed");
+                return null;
+            }
+
+            offset += blockLength;
+            index += blockLength;
+            blockNumber++;
+        }
+
+        pictureBuffer = buffer.toByteArray();
+
+        while (index >= 0 && pictureBuffer[index - 1] == 0x20) index--;
+
+        return BitmapFactory.decodeByteArray(pictureBuffer, 0, index);
+    }
+
+    public boolean verifyPinCode(String pin) {
+        SmartCardMessage.DataBlock data;
+        byte[] message, pinBuffer, challengeBuffer;
+
+        if (pin == null || pin.length() != 4) {
+            Log.w(TAG, "Invalid pin code");
+            return false;
+        }
+
+        pinBuffer = pin.getBytes(Charset.forName("TIS620"));
+
+        if (!this.selectAppletExtension()) {
+            Log.d(TAG, "selectAppletExtension fail");
+            return false;
+        }
+
+        // pin challenge
+        message = new byte[]{(byte)0x80, (byte)0xb4, (byte)0x00, (byte)0x00};
+
+        if ((data = this.getCardData(message)) == null) {
+            Log.w(TAG, "Get pin challenge failed");
+            return false;
+        }
+
+        if (data.status != 0 || data.error != 0) {
+            Log.w(TAG, String.format("Invalid pin challenge response [%d][%d][%d][%s]", data.status, data.error, data.data.length, this.byteArrayToHexString(data.data)));
+            return false;
+        }
+
+        if (data.data.length != 32) {
+            Log.w(TAG, String.format("Invalid pin challenge length [%d]", data.data.length));
+            return false;
+        }
+
+        // TODO: calculate pin challenge response
+
+        // verify pin
+        message = new byte[]{(byte)0x80, (byte)0x20, (byte)0x01, (byte)0x00, (byte)0x20,
+                (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00,
+                (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00,
+                (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00,
+                (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00};
+
+        if ((data = this.getCardData(message)) == null) {
+            Log.w(TAG, "Get pin challenge failed");
+            return false;
+        }
+
+        if (data.status != 0 || data.error != 0) {
+            Log.w(TAG, String.format("Invalid verify pin response [%d][%d][%d][%s]", data.status, data.error, data.data.length, this.byteArrayToHexString(data.data)));
+            return false;
+        }
+
+        if (data.data[0] == (byte)0x63) {
+            Log.w(TAG, String.format("PIN verify incorrect, remaining [%d]", data.data[1]));
+            return false;
+        } else if (data.data[0] == (byte)0x90) {
+            return true;
+        } else {
+            Log.w(TAG, String.format("Invalid verify pin response data [%d][%s]", data.data.length, this.byteArrayToHexString(data.data)));
+            return false;
+        }
     }
 }
